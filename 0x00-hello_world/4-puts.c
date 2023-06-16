1f4a4c41464dce268f012e040f1210c05a4c2aef
youssef <youssef.mossaddak@outlook.fr>
2023-06-15 19:45:45 -0700

    ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -6,7 +7,6 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle\n");
-    return (0);
+{	puts(""Programming is like building a multilingual puzzle");
+return (0);
 }

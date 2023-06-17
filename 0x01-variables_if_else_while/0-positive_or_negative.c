6dd9536f3ae65d08961babf7893f2045fa6cf8b0
youssef <youssef.mossaddak@outlook.fr>
2023-06-16 18:41:39 -0700

    next

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,12 +12,13 @@ int main(void)
         if (n>=0)
         {
                 printf("%d is positive\n",n);
-        } else {
-                printf("%d is negative\n",n);
-        }
+        } 
+        else {
+                printf ("%d is negative\n",n);
+        } 
         if (n=0)
-        {
-                printf("%d is zero\n,n);"
+        { 
+                printf ("%d is zero\n",n);
         }
 
         return (0);

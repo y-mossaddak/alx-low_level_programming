da3b1bb475ace3ef18673bcfb3213ef2a4f0d4cd
youssef <youssef.mossaddak@outlook.fr>
2023-06-20 02:35:21 -0700

    next

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,18 @@
-#include <stdio.h>
+int _putchar(char c)
+{
+    return putchar(c);
+}
+
 int main()
 {
-    printf("_putchar");
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
     return 0;
 }

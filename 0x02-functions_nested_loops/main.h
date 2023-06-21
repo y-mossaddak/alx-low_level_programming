eba2b0107de5bddc2412a4b913afdb04fe770916
youssef <youssef.mossaddak@outlook.fr>
2023-06-20 18:55:12 -0700

    last

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
+void print_to_98(int);
+void times_table(void);
+void print_times_table(int);
+#endif

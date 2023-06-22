f3b7f9eb43e750ecb188f1ae90f8d9898d15b306
youssef <youssef.mossaddak@outlook.fr>
2023-06-22 16:57:02 -0700

    next

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
  * print_numbers - Prints the numbers from 0 to 9
  */
 void print_numbers(void)
 {
 	int i;
-	
+
 	for (i = 48; i < 58; i++)
 	{
 		_putchar(i);
 	}
+
 	_putchar('\n');
 }

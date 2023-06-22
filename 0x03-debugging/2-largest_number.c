b89bd21e27c92efe28cb103bc383c166a13caaf8
youssef <youssef.mossaddak@outlook.fr>
2023-06-21 20:05:31 -0700

    next

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,19 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
+
 	else if (b >= a && b >= c)
 	{
 		largest = b;
-	{
+	}
 	else
 	{
-	largest = c;
+		largest = c;
 	}
-	
+
 	return (largest);
 }	

e50894e5772d0d1755bc77f35bcd8f92d0f82064
youssef <youssef.mossaddak@outlook.fr>
2023-06-22 17:43:35 -0700

    next

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,10 @@ void print_numbers(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		if (i == 50 || i == 52)
+		if (i != 50 && i != 52)
 		{
-			continue;
+			_putchar(i);
 		}
-		_putchar(i);
 	}
 
 	_putchar('\n');

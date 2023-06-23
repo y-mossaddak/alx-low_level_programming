19c834ed9acb025ea8820bf30876376f730f090f
youssef <youssef.mossaddak@outlook.fr>
2023-06-22 19:46:44 -0700

    next

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - FizzBuzz
+ * Description: FizzBuzz game.
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+		if (n < 100)
+			printf(" ");
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
+

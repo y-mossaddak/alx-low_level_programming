f39ca52bf1182d011c86956de3c2802daab7f702
youssef <youssef.mossaddak@outlook.fr>
2023-07-07 17:42:49 -0700

    next

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,14 +5,12 @@
  * main - the entry point
  * @argc: the number of arguments
  * @argv: array of string arguments
- *
- * Description: Prints the number of arguments passed into it
- *
- * Return: Always 0
+ * Description:  prints the number of arguments passed into it
+ * Return: always 0
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-    printf("%d\n", argc);
-    return 0;
+	printf("%d\n", argc);
+	return (0);
 }
 

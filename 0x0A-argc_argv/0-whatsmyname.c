aba770bc4d9caa0f1c39d241c7190ecd6e99e4a0
youssef <youssef.mossaddak@outlook.fr>
2023-07-07 17:05:36 -0700

    next

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include "main.h"
+
+/**
+ * main - the entry point
+ * @argc: the number of arguments
+ * @argv: array of string arguments
+ * Description:program to print its path name
+ * Return: always 0
+ */
 
 int main(int argc, char *argv[])
 {

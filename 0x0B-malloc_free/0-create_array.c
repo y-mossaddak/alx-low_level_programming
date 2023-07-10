09aaf594f9225f3f73bbd35e8275c4f9868490e8
youssef <youssef.mossaddak@outlook.fr>
2023-07-10 16:12:03 -0700

    next

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,7 +6,7 @@
  * create_array: creates an array of chars
  * @size: space reserved in memory
  * @c: character
- *
+ * description: creates an array of chars, and initializes it with a specific char.
  * Returns: A adress of array value
  */
 char *create_array(unsigned int size, char c)

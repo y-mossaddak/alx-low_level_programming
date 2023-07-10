a5ebfe95db06ad984d4fd613f69f9fe43e5dbd0e
youssef <youssef.mossaddak@outlook.fr>
2023-07-10 15:27:00 -0700

    next

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-/**
- * @create_array : creates an array of chars
- * @size : space reserved in memory
- * @c : character
- * return : adress reserved in memory
- */
 
+/**
+ * create_array: creates an array of chars
+ * @size: space reserved in memory
+ * @c: character
+ *
+ * Returns: address of the reserved memory
+ */
 char *create_array(unsigned int size, char c)
 {
-	if (size == NULL)
-	{
-		printf("failed to allocate memory\n");
-		return (NULL);
-	}
 	if (size == 0)
 	{
-		return (NULL);
-	}
-	int i = 0;
-	char A[size];
-	for (i; i < size; i++)
-	{
-		A[i] = i + 1;
+		printf("Failed to allocate memory. Size must be greater than zero.\n");
+		return NULL;
 	}
 
-	return (create_array);
+	char *A = malloc(size * sizeof(char));
+	if (A == NULL)
+	{
+		printf("Failed to allocate memory.\n");
+		return NULL;
+	}
+
+	for (unsigned int i = 0; i < size; i++)
+	{
+		A[i] = c;
+	}
+
+	return A;
 }

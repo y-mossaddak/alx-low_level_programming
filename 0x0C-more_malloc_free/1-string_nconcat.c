48f926610d00d223216dad779fd02777295e13ff
youssef <youssef.mossaddak@outlook.fr>
2023-07-13 16:14:36 -0700

    next

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,57 +1,59 @@
-#include <stdlib.h>
 #include "main.h"
-/**
- * string_nconcat - Concatenates two strings up to n characters.
- * @s1: The first string.
- * @s2: The second string.
- * @n: The maximum number of characters from s2 to concatenate.
- *
- * Return: Pointer to the concatenated string, or NULL on failure.
- */
+#include <stdio.h>
+#include <stdlib.h>
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+/**
+ * str_concat - concat twoo strings
+ * @s1: String source 01
+ * @s2: String source 02
+ *
+ * Return: string pointer
+ */
+char *str_concat(char *s1, char *s2)
 {
-	int length1, length2, a, b, c, i;
-	length1 = strlen(s1);
-	length2 = strlen(s2);
-	a = length1 + length2;
-	b = length1 + n;
-	char *two_strings;
+	char *new_string;
+	unsigned int i, j, k, limit;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n >= length2)
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	if (n >= j)
 	{
-		two_strings = malloc(a + 1);
-		if (two_strings == NULL)
-		{
+	two_strings = malloc(sizeof(char)*(i + j + 1);
+        if(two_strings == NULL)
+            {
 			free(two_strings);
 			return (NULL);
-		}
+            }
 	}
 	else
 	{
-		two_strings = malloc(b + 1);
-		if (two_strings == NULL)
-		{
-			free(two_strings);
-			return (NULL);
-		}
-	}
-	for (c = 0; c < length1; c++)
-		two_stringers[c] = s1[c];
-
-	if (n >= length2)
+	two_strings = malloc(sizeof(char) * (i + n + 1));
+	if (two_strings == NULL)
 	{
-		for (i = 0; i < length2; c++; i++)
-			two_strings[c] = s2[i];
+		free(two_strings);
+		return (NULL);
+	}
+	}
+	for (k = 0; k < i; k++)
+	two_stringers[k] = s1[k];
+
+	if (n >= j)
+	{
+	for (i = 0; i < j; k++; i++)
+		two_strings[k] = s2[i];
 	}
 	else
 	{
-		for (i = 0; i < n; c++; i++)
-			two_strings[c] = s2[i];
-
-		return (two_strings);
+	for (i = 0; i < n; k++; i++)
+		two_strings[k] = s2[i];
 	}
+	return (two_strings);
+}

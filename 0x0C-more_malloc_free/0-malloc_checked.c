5cdc13a2e5cd48d83f954a1937ab793e67a95be8
youssef <youssef.mossaddak@outlook.fr>
2023-07-13 06:28:03 -0700

    next

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: Size of the memory to allocate
+ *
+ * Return: Pointer to the allocated memory
+ *         Exit with status 98 on failure
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *s;
+	s = malloc(b);
+
+	if (s == NULL)
+	{
+		exit(98);
+	}
+	return (s);
+}

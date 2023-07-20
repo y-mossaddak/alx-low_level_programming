cf92a1873ec2d23f0e3b27406ff7dcb7ba11df52
youssef <youssef.mossaddak@outlook.fr>
2023-07-20 05:43:42 -0700

    next

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,7 +9,7 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	int i = n
+	int i = n;
 	va_list args;
 	if (n == 0)
 	{
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	while (i--)
 	{
 		printf("%d%s",va_arg(args, int));
-		i?(separator?separator:""):"\n";
+		i ? ((separator ? separator :"") : "\n");
 	}
 
 	va_end(args);

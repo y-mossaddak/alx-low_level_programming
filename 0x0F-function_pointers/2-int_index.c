f2e665b26e3c9fcc73671878d9156b918131af09
youssef <youssef.mossaddak@outlook.fr>
2023-07-19 19:18:14 -0700

    next

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - find index
+ * @array: array to map
+ * @size: size of array
+ * @cmp: function to search
+ * Return: Void
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && size && cmp)
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
+
+	return (-1);
+}

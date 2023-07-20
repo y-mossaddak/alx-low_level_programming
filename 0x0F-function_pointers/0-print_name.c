f32f33fecfe21e32a103bb2eabee971b1c98806a
youssef <youssef.mossaddak@outlook.fr>
2023-07-19 19:34:22 -0700

    next

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,6 @@
  * print_name - Prints a name using a function pointer
  * @name: The name to be printed
  * @f: A pointer to a function that will print the name
- *
  * Description: This function takes a name and a function pointer as parameters
  *              and calls the function pointed to by the function pointer to
  *              print the name.

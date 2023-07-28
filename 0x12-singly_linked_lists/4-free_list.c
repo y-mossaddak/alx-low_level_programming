cf18fdf11f810dd11cb4b729b2359ad340686de9
youssef <youssef.mossaddak@outlook.fr>
2023-07-27 20:03:15 -0700

    next

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,23 +1,18 @@
 #include "lists.h"
-
 /**
- * list_len - Return the number of elements in a singly linked list.
- * @h: A pointer to the head of the linked list.
-/**
- * free_list - Free memory allocated for a singly linked list.
- * @head: Pointer to the head of the linked list.
+ * free_list - free all node mem
+ * @head: list head
+ * Return: void
  */
 void free_list(list_t *head)
 {
-    list_t *current = head;
-    list_t *next;
+	list_t *p;
 
-    while (current != NULL)
-    {
-        next = current->next;
-        free(current->str);
-        free(current);
-        current = next;
-    }
+	while ((p = head) != NULL)
+	{
+		head = head->next;
+		free(p->str);
+		free(p);
+	}
 }
 

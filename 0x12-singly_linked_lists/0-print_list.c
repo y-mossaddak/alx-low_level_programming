608c0d7a565e7f432206cb4f45e30f075c4dea05
youssef <youssef.mossaddak@outlook.fr>
2023-07-27 19:07:29 -0700

    next

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,18 +1,21 @@
 #include "lists.h"
+
 /**
- * print_list - Print the elements of a singly linked list.
- * @h: A pointer to the head of the linked list.
- * Return:  the number of elements in a linked list_t list
+ * print_list - print a sigle liste
+ * @h: Liste head
+ * Return: size_t signe liste node count
  */
 size_t print_list(const list_t *h)
 {
-	size_t counter = 0;
+	size_t count = 0;
+
 	while (h)
 	{
-		printf("[%d] ", h->len ? h->len : [0]);
+		printf("[%d] ", h->str ? h->len : 0);
 		printf("%s\n", h->str ? h->str : "(nil)");
 		h = h->next;
-		counter++;
+		count++;
 	}
-	return (counter);
+	return (count);
 }
+

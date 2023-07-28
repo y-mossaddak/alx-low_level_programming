d835f05dffab46a0e2fae00404173ded77bad795
youssef <youssef.mossaddak@outlook.fr>
2023-07-27 19:20:06 -0700

    next

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,18 +1,19 @@
 #include "lists.h"
+
 /**
- * list_len - return the numbers of elements of a singly linked list.
+ * list_len - Return the number of elements in a singly linked list.
  * @h: A pointer to the head of the linked list.
- * Return:  the number of elements in a linked list_t list
+ *
+ * Return: The number of elements in the linked list.
  */
-size_t list_len(const list_t *h);
+size_t list_len(const list_t *h)
 {
+	size_t counter = 0;
+	while (h)
 	{
-		size_t counter = 0;
-		while (h)
-		{
-			h = h->next;
-			counter++;
-		}
-		return (counter);
+		h = h->next;
+		counter++;
 	}
+	return counter;
 }
+
